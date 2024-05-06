3124ee0c235d1a8aa05b0fb69a763700eea4b8da
coverj <win8yh1@outlook.com>
2024-05-06 18:08:08 +0800

    feat: 添加unocss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import '@/assets/styles/index.scss'
 import router from './router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import 'virtual:uno.css'
 
 createApp(App).use(router).use(createPinia()).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
@@ -9,6 +10,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
     }),
