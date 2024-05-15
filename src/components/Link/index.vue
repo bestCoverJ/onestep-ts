7556ee4717d3ba52d4070c61489c9b05e867fc1e
coverj <win8yh1@outlook.com>
2024-05-15 22:13:08 +0800

    fix: 优化移动端显示

---
+++ src/components/Link/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="component-link bg-white/30"
+    grid
+    grid-cols-3
+    grid-rows-3
+    box-border
+    w-full
+    h-full
+    p-4
+    backdrop-blur-xl
+    rounded-lg
+  ></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
--- src/view/index.vue
+++ src/view/index.vue
@@ -3,15 +3,16 @@
     <indexLayout>
       <template #default>
         <Search class="col-span-3 md:col-span-4 lg:col-span-5"></Search>
-        <Hitokoto></Hitokoto>
-        <Weather2></Weather2>
-        <Collect></Collect>
+        <Hitokoto v-if="isLargeScreen"></Hitokoto>
+        <Weather2 v-if="isLargeScreen"></Weather2>
+        <Collect v-if="isLargeScreen"></Collect>
+        <Link v-if="isLargeScreen"></Link>
       </template>
       <template #footer>
-        <span class="text-stone-900">OneStep 2024</span>
+        <span v-if="isLargeScreen" class="text-stone-900">OneStep 2024</span>
       </template>
       <template #topRight>
-        <!-- <Setting></Setting> -->
+        <Setting v-if="isLargeScreen"></Setting>
       </template>
     </indexLayout>
   </div>
@@ -24,7 +25,12 @@ import Search from '@/components/Search/index.vue'
 import Hitokoto from '@/components/Hitokoto/index.vue'
 import Weather2 from '@/components/Weather2/index.vue'
 import Collect from '@/components/Collect/index.vue'
-// import Setting from '@/components/Setting/index.vue'
+import Link from '@/components/Link/index.vue'
+import Setting from '@/components/Setting/index.vue'
+
+import { useMediaQuery } from '@vueuse/core'
+
+const isLargeScreen = useMediaQuery('(min-width: 1024px)')
 </script>
 
 <style lang="scss">
